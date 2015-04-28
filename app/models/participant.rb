5eaaf40b26ed112c527cf7b3c15f53e2e122e9df
Maarten Brouwers <github.com@murb.nl>
2015-04-28 23:20:04 +0200

    test

--- app/models/participant.rb
+++ app/models/participant.rb
@@ -1,6 +1,6 @@
 class Participant < ActiveRecord::Base
   before_create :hash_participant!
-
+  has_many :responses
 
   def hash_participant!
     self.participant_hash = Digest::SHA2.new(256).update("#{self.serializable_hash}+#{Time.now}+jibrrrji!@#sh.+#{session}").to_s
