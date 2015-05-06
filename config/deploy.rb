7726bb67a568193694303e2ec869d62a8efe84a8
Maarten Brouwers <github.com@murb.nl>
2015-05-06 20:12:02 +0200

    deploy

---
+++ config/deploy.rb
@@ -0,0 +1,62 @@
+# config valid only for current version of Capistrano
+lock '3.3.5'
+
+set :application, 'cat.murb.nl'
+set :repo_url, 'cat@murb.nl:/home/cat/source-catm.git'
+
+
+set :bundle_bins, []
+set :bundle_roles, :all
+set :bundle_binstubs, -> { shared_path.join('bin') }
+set :bundle_gemfile, -> { release_path.join('Gemfile') }
+set :bundle_path, -> { shared_path.join('bundle') }
+set :bundle_without, %w{development test}.join(' ')
+set :bundle_flags, ''
+
+set :rbenv_type, :user # or :system, depends on your rbenv setup
+set :rbenv_ruby, '2.2.2'
+set :rbenv_roles, :all # default value
+
+
+# Default branch is :master
+# ask :branch, proc { `git rev-parse --abbrev-ref HEAD`.chomp }.call
+
+# Default deploy_to directory is /var/www/my_app_name
+set :deploy_to, '~/public/cat.murb.nl'
+
+# Default value for :scm is :git
+# set :scm, :git
+
+# Default value for :format is :pretty
+# set :format, :pretty
+
+# Default value for :log_level is :debug
+# set :log_level, :debug
+
+# Default value for :pty is false
+# set :pty, true
+
+# Default value for :linked_files is []
+# set :linked_files, fetch(:linked_files, []).push('config/database.yml')
+
+# Default value for linked_dirs is []
+# set :linked_dirs, fetch(:linked_dirs, []).push('bin', 'log', 'tmp/pids', 'tmp/cache', 'tmp/sockets', 'vendor/bundle', 'public/system')
+
+# Default value for default_env is {}
+# set :default_env, { path: "/opt/ruby/bin:$PATH" }
+
+# Default value for keep_releases is 5
+# set :keep_releases, 5
+
+namespace :deploy do
+
+  after :restart, :clear_cache do
+    on roles(:web), in: :groups, limit: 3, wait: 10 do
+      # Here we can do anything such as:
+      # within release_path do
+      #   execute :rake, 'cache:clear'
+      # end
+    end
+  end
+
+end
