2fc0fb6f1d5fc8ca7a78458241ba7cf4730a152f
Maarten Brouwers <github.com@murb.nl>
2015-08-29 23:05:23 +0200

    uploads

--- config/deploy.rb
+++ config/deploy.rb
@@ -40,7 +40,7 @@ set :deploy_to, '~/public/cat.murb.nl'
 set :linked_files, fetch(:linked_files, []).push('db/testing.sqlite3')
 
 # Default value for linked_dirs is []
-set :linked_dirs, fetch(:linked_dirs, []).push('bin', 'log', 'tmp/pids', 'tmp/cache', 'tmp/sockets', 'vendor/bundle', 'public/system')
+set :linked_dirs, fetch(:linked_dirs, []).push('bin', 'log', 'tmp/pids', 'tmp/cache', 'tmp/sockets', 'vendor/bundle', 'public/system', 'public/uploads')
 
 # Default value for default_env is {}
 # set :default_env, { path: "/opt/ruby/bin:$PATH" }
